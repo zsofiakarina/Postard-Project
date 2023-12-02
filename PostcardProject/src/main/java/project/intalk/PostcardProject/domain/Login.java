419e53c14a033bf13cb99af51dcab0d5a8f430b3
csanadpleli <pleli.csanad@gmail.com>
2023-12-02 15:29:07 +0100

    login

--- PostcardProject/src/main/java/project/intalk/PostcardProject/domain/Login.java
+++ PostcardProject/src/main/java/project/intalk/PostcardProject/domain/Login.java
@@ -1,4 +1,29 @@
 package project.intalk.PostcardProject.domain;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Login {
+
+    @NotNull(message = "Provide valid data!")
+    @Size(min = 4, max = 18)
+    private String username;
+
+    @NotNull(message = "Provide valid data!")
+    @Size(min = 6, max = 12)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
