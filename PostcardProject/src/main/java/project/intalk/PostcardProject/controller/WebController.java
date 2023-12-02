91dc9428b11a23922be23d59d23ee91e7abc4a06
zsofiakarina <zsofia.karina19@gmail.com>
2023-12-02 14:37:17 +0100

    Personas

--- PostcardProject/src/main/java/project/intalk/PostcardProject/controller/WebController.java
+++ PostcardProject/src/main/java/project/intalk/PostcardProject/controller/WebController.java
@@ -1,4 +1,23 @@
 package project.intalk.PostcardProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.intalk.PostcardProject.personas.User;
+import project.intalk.PostcardProject.repository.UserRepository;
+
+@RestController
+@RequestMapping("/auth")
 public class WebController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+
+        return userRepository.save(user);
+    }
 }
