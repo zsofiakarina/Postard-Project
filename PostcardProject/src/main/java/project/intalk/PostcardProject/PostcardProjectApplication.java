8c3166d5e822327646cae22d051a64f8f9077d0f
zsofiakarina <47834658+zsofiakarina@users.noreply.github.com>
2023-12-02 13:22:52 +0100

    Add files via upload

---
+++ PostcardProject/src/main/java/project/intalk/PostcardProject/PostcardProjectApplication.java
@@ -0,0 +1,13 @@
+package project.intalk.PostcardProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcardProjectApplication.class, args);
+	}
+
+}
---
+++ PostcardProject/src/test/java/project/intalk/PostcardProject/PostcardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.intalk.PostcardProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
