beed55c66e17ba8bfb3250174b8c0262a41883e9
csanadpleli <pleli.csanad@gmail.com>
2023-12-10 23:13:36 +0100

    comments

--- PostcardProject/src/main/java/project/intalk/PostcardProject/repository/PostcardRepository.java
+++ PostcardProject/src/main/java/project/intalk/PostcardProject/repository/PostcardRepository.java
@@ -8,9 +8,10 @@ import java.util.List;
 public interface PostcardRepository extends JpaRepository<Postcard, Long> {
 
     List<Postcard> findAll();
-    List<Postcard> findByName(String name);
 
-    List<Postcard> findByStatusAndName(String status, String name);
+/*    List<Postcard> findByName(String name);
+
+    List<Postcard> findByStatusAndName(String status, String name);*/
 
     List<Postcard> findByStatus(String status);
 }
